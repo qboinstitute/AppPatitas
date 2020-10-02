beecb58c4d57cff73883dd5a72853a57e7c91ac1
LuisAngel <luisangel.salvat@gmail.com>
2020-10-01 20:22:17 -0500

    Módulo 6, Actualización

--- app/src/main/java/qboinstitute/com/apppatitas/adapters/MascotaAdapter.kt
+++ app/src/main/java/qboinstitute/com/apppatitas/adapters/MascotaAdapter.kt
@@ -1,29 +1,45 @@
 package qboinstitute.com.apppatitas.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import qboinstitute.com.apppatitas.R
 import qboinstitute.com.apppatitas.model.Mascota
 
 class MascotaAdapter(private var lstmascotas: List<Mascota>,
 private val context: Context)
-    : RecyclerView.Adapter<RecyclerView.ViewHolder>()
+    : RecyclerView.Adapter<MascotaAdapter.ViewHolder>()
 {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return ViewHolder(layoutInflater.inflate(R.layout.item_mascota,
+            parent, false))
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val itemmascota = lstmascotas[position]
+        holder.tvnommascota.text = itemmascota.nommascota
+        holder.tvlugar.text = itemmascota.lugar
+        holder.tvcontacto.text = itemmascota.contacto
+        holder.tvfecha.text = itemmascota.fechaperdida
+        Glide.with(context).load(itemmascota.urlimagen).into(holder.ivmascota)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return lstmascotas.size
     }
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-
+        val tvnommascota : TextView = itemView.findViewById(R.id.tvnommascota)
+        val tvlugar : TextView = itemView.findViewById(R.id.tvlugar)
+        val tvfecha : TextView = itemView.findViewById(R.id.tvfecha)
+        val tvcontacto : TextView = itemView.findViewById(R.id.tvcontacto)
+        val ivmascota : ImageView = itemView.findViewById(R.id.ivmascota)
     }
 
 }
--- app/src/main/java/qboinstitute/com/apppatitas/ui/ListaMascotaFragment.kt
+++ app/src/main/java/qboinstitute/com/apppatitas/ui/ListaMascotaFragment.kt
@@ -1,22 +1,62 @@
 package qboinstitute.com.apppatitas.ui
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.fragment_lista_mascota.*
 import qboinstitute.com.apppatitas.R
+import qboinstitute.com.apppatitas.adapters.MascotaAdapter
+import qboinstitute.com.apppatitas.model.Mascota
 
 
 class ListaMascotaFragment : Fragment() {
 
+    private lateinit var queue: RequestQueue
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lista_mascota, container, false)
+        queue = Volley.newRequestQueue(context)
+        val vista = inflater.inflate(R.layout.fragment_lista_mascota, container, false)
+        val rvmascota : RecyclerView = vista.findViewById(R.id.rvmascota)
+        rvmascota.layoutManager = LinearLayoutManager(context)
+        ListarMascotasPerdidasWS(vista.context)
+        return vista
+    }
+
+    private fun ListarMascotasPerdidasWS(context: Context) {
+        val lstmascota : ArrayList<Mascota> = ArrayList()
+        val urlwslista = "http://www.kreapps.biz/patitas/mascotaperdida.php"
+        val request = JsonArrayRequest(Request.Method.GET,
+        urlwslista, null, { respose ->
+                for(i in 0 until respose.length()){
+                    val itemjson = respose.getJSONObject(i)
+                    lstmascota.add(
+                        Mascota(
+                            itemjson["nommascota"].toString(),
+                            itemjson["fechaperdida"].toString(),
+                            itemjson["urlimagen"].toString(),
+                            itemjson["lugar"].toString(),
+                            itemjson["contacto"].toString()
+                    ))
+                }
+                rvmascota.adapter = MascotaAdapter(lstmascota, context)
+            }, {
+                Log.e("LISTAR", it.message)
+            })
+        queue.add(request)
     }
 
 }
